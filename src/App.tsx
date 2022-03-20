c9f08cf6e03ae4d46782d1061c060806ffd07790
Kodake <kodake@hotmail.es>
2022-03-20 01:11:00 -0400

    All Initial changes about multipage-app

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Route, Switch, Link, NavLink } from 'react-router-dom';
+// page components
+import Home from './pages/Home';
+import Contact from './pages/Contact';
+import About from './pages/About';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <nav>
+          <h1>My Articles</h1>
+          <NavLink exact to='/'>Home</NavLink>
+          <NavLink to='/about'>About</NavLink>
+          <NavLink to='/contact'>Contact</NavLink>
+        </nav>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+
+          <Route path="/about">
+            <About />
+          </Route>
+
+          <Route path="/contact">
+            <Contact />
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/pages/About.tsx
@@ -0,0 +1,13 @@
+const About = () => {
+    return (
+        <div>
+            <h2>Aboutpage</h2>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                Minus eius ut a fugit reprehenderit. Natus, maxime. Modi
+                suscipit neque at autem quisquam quaerat, veniam aut quibusdam
+                vero ducimus qui reprehenderit!</p>
+        </div>
+    )
+}
+
+export default About
